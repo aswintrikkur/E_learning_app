21f99ac20c84ee61c5072689aa39eb627aaff526
Aswinkumar <aswin.trikkur@gmail.com>
2025-03-26 21:33:06 +0530

    vercel.json added

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_DOMAIN, credentials: true }));
 
 connectDB();
 
